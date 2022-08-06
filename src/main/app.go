876e45b5f8beb698dfb60d2b6706b67c3dad2a08
montlein <montvlein@gmail.com>
2022-08-05 23:36:14 -0400

    creando un bot basico

--- src/main/app.go
+++ src/main/app.go
@@ -16,6 +16,36 @@ func init() {
 }
 
 func main() {
-	token := os.Getenv("TELEGRAM_BOT_TOKEN")
-	log.Println(token)
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 30
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		var textResp string
+		switch update.Message.Text {
+		case "/start":
+			textResp = "bienvenido"
+		case "/config":
+			textResp = "cargando config"
+		default:
+			textResp = "respuesta generica"
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, textResp)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Println(err)
+		}
+	}
 }
