9fdece47454f6f1850357f71203e8854ea5a8fe0
montlein <montvlein@gmail.com>
2022-08-05 23:28:20 -0400

    install module godotenv & load .env file

--- src/main/app.go
+++ src/main/app.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
-	log.Println("iniciando proyecto")
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	log.Println(token)
 }
