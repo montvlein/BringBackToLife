98c3a979047c58917af08780a23521c998421550
montlein <montvlein@gmail.com>
2022-08-06 21:58:52 -0400

    se formatearon las respuestas y se agrego un menu desplegable

--- src/main/app.go
+++ src/main/app.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
@@ -35,6 +37,12 @@ func main() {
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, textResp)
 		msg.ReplyToMessageID = update.Message.MessageID
+		switch update.Message.Text {
+		case "/start", "open":
+			msg.ReplyMarkup = optionKeyboard
+		case "close":
+			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+		}
 
 		if _, err := bot.Send(msg); err != nil {
 			log.Println(err)
@@ -42,25 +50,50 @@ func main() {
 	}
 }
 
-func start() string {
-	return "bienvenido"
-}
-
-func showConfig() string {
-	return "mostrando configuracion"
-}
-
 func armarRespuesta(mensajeRecibido *string) string {
 	var respuesta string
 	switch *mensajeRecibido {
 	case "/start":
-		respuesta = start()
-	case "/config":
-		respuesta = showConfig()
-	case "ctd":
-		respuesta = "FRONTEND 3 - lunes, miercoles, jueves 18hsARG / 16hsCO"
+		respuesta = start
+		count = 3
+	case "help":
+		respuesta = help
+		count = 3
+	case "calendar":
+		respuesta = os.Getenv("CALENDAR")
+		count = 3
+	case "link":
+		respuesta = fmt.Sprintf("link:\n%v\npass:\n%v", os.Getenv("LINK"), os.Getenv("LINK_PASS"))
+		count = 3
+	case "open":
+		respuesta = "menu abierto"
+	case "close":
+		respuesta = "menu cerrado"
+
 	default:
-		respuesta = "respuesta generica"
+		if count > 0 {
+			count--
+			respuesta = "No entiendo la consulta... \nEste mensaje se mostrara hasta " + strconv.Itoa(int(count)) + " veces seguidas mas"
+		}
 	}
 	return respuesta
 }
+
+var optionKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("help"),
+		tgbotapi.NewKeyboardButton("link"),
+		tgbotapi.NewKeyboardButton("calendar"),
+	),
+)
+
+var start string = "BIENVENIDE\n\n" + help
+
+var help string = "Comandos de ayuda\n" +
+	"help - muestra la lista de comandos\n" +
+	"open - abre el menu de opciones/teclado rapido\n" +
+	"close - cierra el menu de opciones/teclado rapido\n" +
+	"link - te dara el link de los encuentros de zoom\n" +
+	"calendar - te mostrara las fechas importantes de la materia\n"
+
+var count int8 = 3
