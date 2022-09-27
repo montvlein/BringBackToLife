5bb0fabbcdd3f0e803521d4efdcb1317380bd92c
montlein <montvlein@gmail.com>
2022-09-27 12:56:19 -0400

    se agrego conexion a api de google calendar

--- src/main/app.go
+++ src/main/app.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
+	"time"
+
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
@@ -13,11 +19,30 @@ import (
 func init() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file\n v%", err.Error())
 	}
 }
 
+const (
+	CALENDAR string = "FRONTEND 3 - lunes, miercoles, jueves 18hsARG / 16hsCO"
+	LINK string = "https://digitalhouse.zoom.us/my/aulavirtual94"
+	start string = "BIENVENIDE\n\n" + help
+	help string = "Comandos de ayuda\n" +
+	"help - muestra la lista de comandos\n" +
+	"open - abre el menu de opciones/teclado rapido\n" +
+	"close - cierra el menu de opciones/teclado rapido\n" +
+	"link - te dara el link de los encuentros de zoom\n" +
+	"calendar - te mostrara las fechas importantes de la materia\n"
+)
+
+var count int8 = 3
+var textResp string
+
 func main() {
+	runBot()
+}
+
+func runBot() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
 		panic(err)
@@ -33,7 +58,7 @@ func main() {
 			continue
 		}
 
-		var textResp = armarRespuesta(&update.Message.Text)
+		textResp = armarRespuesta(&update.Message.Text)
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, textResp)
 		msg.ReplyToMessageID = update.Message.MessageID
@@ -52,7 +77,7 @@ func main() {
 
 func armarRespuesta(mensajeRecibido *string) string {
 	var respuesta string
-	switch *mensajeRecibido {
+	switch strings.ToLower( *mensajeRecibido ){
 	case "/start":
 		respuesta = start
 		count = 3
@@ -60,10 +85,10 @@ func armarRespuesta(mensajeRecibido *string) string {
 		respuesta = help
 		count = 3
 	case "calendar":
-		respuesta = os.Getenv("CALENDAR")
+		respuesta = getEvents(GetOauthAndCalendar())
 		count = 3
 	case "link":
-		respuesta = fmt.Sprintf("link:\n%v\npass:\n%v", os.Getenv("LINK"), os.Getenv("LINK_PASS"))
+		respuesta = fmt.Sprintf("link:\n%v\npass:\n%v", LINK, os.Getenv("LINK_PASS"))
 		count = 3
 	case "open":
 		respuesta = "menu abierto"
@@ -82,18 +107,45 @@ func armarRespuesta(mensajeRecibido *string) string {
 var optionKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
 		tgbotapi.NewKeyboardButton("help"),
-		tgbotapi.NewKeyboardButton("link"),
 		tgbotapi.NewKeyboardButton("calendar"),
+		tgbotapi.NewKeyboardButton("close"),
 	),
 )
 
-var start string = "BIENVENIDE\n\n" + help
+func GetOauthAndCalendar() (*calendar.Service){
+	ctx := context.Background()
+	service, err := calendar.NewService(ctx, option.WithCredentialsFile("bbtl-api-conection.json"))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+	return service
+}
 
-var help string = "Comandos de ayuda\n" +
-	"help - muestra la lista de comandos\n" +
-	"open - abre el menu de opciones/teclado rapido\n" +
-	"close - cierra el menu de opciones/teclado rapido\n" +
-	"link - te dara el link de los encuentros de zoom\n" +
-	"calendar - te mostrara las fechas importantes de la materia\n"
-
-var count int8 = 3
+func getEvents(service *calendar.Service) string {
+	t := time.Now().Format(time.RFC3339)
+	events, err := service.Events.List(os.Getenv("calendarId")).ShowDeleted(false).
+		SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+	}
+	fmt.Println("Upcoming events:")
+	var info string
+	if len(events.Items) == 0 {
+		fmt.Println("No upcoming events found.")
+		info = "No upcoming events found."
+	} else {
+		for _, item := range events.Items {
+			date := item.Start.DateTime
+			d, err := time.Parse(time.RFC3339, date)
+			if err != nil {
+				log.Fatalf("Error al parsear el tiempo: %v", err)
+			}
+			if date == "" {
+				date = item.Start.Date
+			}
+			fmt.Printf("%v (%v)\n", item.Summary, d.Format(time.RFC850))
+			info = strings.Join([]string{info, item.Summary, d.Format(time.RFC850), "\n"}, " ")
+		}
+	}
+	return info
+}
